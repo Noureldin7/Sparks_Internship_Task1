52c644a88ae1c7a5da4474f0604ef708ef5554c2
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg>
2023-03-04 13:13:03 +0200

    sql + test route

--- api/controller.js
+++ api/controller.js
@@ -1,5 +1,17 @@
+const db = require("./../sql")
 exports.getCustomers = async (req,res) => {
-    res.status(200).json({
-        success:'true'
+    db.query('Select * from customers',(err,results)=>{
+        if(err){
+            console.log(err.sqlMessage)
+            res.status(200).json({
+                error:err.sqlMessage
+            }) 
+        }
+        else
+        {
+            res.status(200).json({
+                results:results
+            }) 
+        }
     })
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require("http");
-require("dotenv")
+require("dotenv").config()
 const app = require("./app")
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+
+
 server.listen(port,()=>{
     console.log(`Server running on port ${port}`);
 })
