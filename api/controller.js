4a742e0efce91cb10899373400916306ef88c10c
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg>
2023-03-04 13:54:36 +0200

    Customer Search + Retrieval

--- api/controller.js
+++ api/controller.js
@@ -1,17 +1,64 @@
 const db = require("./../sql")
 exports.getCustomers = async (req,res) => {
-    db.query('Select * from customers',(err,results)=>{
+    db.query(`Select * from Customers`,(err,results,fields)=>{
         if(err){
-            console.log(err.sqlMessage)
-            res.status(200).json({
+            res.status(400).json({
                 error:err.sqlMessage
-            }) 
+            })
         }
         else
         {
             res.status(200).json({
-                results:results
-            }) 
+                result:results
+            })
+        }
+    })
+}
+exports.getCustomerById = async (req,res) => {
+    const id = req.params.id
+    db.query(`Select * from Customers Where id=${id}`,(err,results,fields)=>{
+        if(err){
+            res.status(400).json({
+                error:err.sqlMessage
+            })
+        }
+        else
+        {
+            res.status(200).json({
+                result:results
+            })
+        }
+    })
+}
+exports.getCustomerExceptId = async (req,res) => {
+    const id = req.params.id
+    db.query(`Select id,name from Customers Where id!=${id}`,(err,results,fields)=>{
+        if(err){
+            res.status(400).json({
+                error:err.sqlMessage
+            })
+        }
+        else
+        {
+            res.status(200).json({
+                result:results
+            })
+        }
+    })
+}
+exports.searchCustomerByName = async (req,res) => {
+    const name = req.query.name
+    db.query(`Select id,name from Customers Where name like '%${name}%'`,(err,results,fields)=>{
+        if(err){
+            res.status(400).json({
+                error:err.sqlMessage
+            })
+        }
+        else
+        {
+            res.status(200).json({
+                result:results
+            })
         }
     })
 }
--- api/routes.js
+++ api/routes.js
@@ -2,4 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
 router.get('/view',controller.getCustomers);
+router.get('/view/:id',controller.getCustomerById);
+router.get('/transfer/:id',controller.getCustomerExceptId);
+router.get('/search',controller.searchCustomerByName);
 module.exports = router
