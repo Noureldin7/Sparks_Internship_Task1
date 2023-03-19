f954ce02d861950083a9b38f14eee1a811f81ce1
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg>
2023-03-19 20:00:03 +0200

    Frontend Build

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cors = require("cors")
+const path = require("path")
 const app = express();
 const routes = require("./api/routes")
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'build')))
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"build","index.html"))
+})
 app.use('/api',routes)
 module.exports = app;
