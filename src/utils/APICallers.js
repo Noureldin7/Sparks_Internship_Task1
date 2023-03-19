7d8e9784d0192cedf0dc0b295eaf33b9172d1ef2
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg>
2023-03-19 08:23:44 +0200

    APICallers

---
+++ src/utils/APICallers.js
@@ -0,0 +1,33 @@
+export async function post(url,payload){
+    const response = await fetch(url,{
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+    return response;
+}
+export async function put(url,payload){
+    const response = await fetch(url,{
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+    return response;
+}
+export async function get(url,params){
+    const urlExtend = params?"?"+new URLSearchParams(params):""
+    const response = await fetch(url+urlExtend,{
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type':'application/json'
+        }
+    });
+    return response;
+}
