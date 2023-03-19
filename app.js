e0dec7b02eb505d8653804530f3e8edb7f3853f7
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg>
2023-03-19 20:48:29 +0200

    route fix

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const routes = require("./api/routes")
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'build')))
+app.use('/api',routes)
 app.get("/*",(req,res)=>{
     res.sendFile(path.join(__dirname,"build","index.html"))
 })
-app.use('/api',routes)
 module.exports = app;
